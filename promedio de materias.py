74f2017ece5a4cc76690601f14ac2efd76410981
britezeli9 <elibritez_686@hotmail.com>
2024-06-07 09:02:48 -0300

    ejercicios python

---
+++ promedio de materias.py
@@ -0,0 +1,11 @@
+#promedio de materias
+print("---------------------------")
+materia1= int(input("ingrese su 1 nota "))
+materia2= int(input("ingrese su 2 nota "))
+materia3= int(input("ingrese su 3 nota "))
+materia4= int(input("ingrese su 4 nota "))
+materia5= int(input("ingrese su 5 nota "))
+
+promedio=((materia1+ materia2+ materia3 + materia4 + materia5)/5)
+print("su promedio de cursada es",promedio)
+print ("----------------------------------")
